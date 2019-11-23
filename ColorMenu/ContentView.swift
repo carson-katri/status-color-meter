18876048501639a451ba620c03ec840adcc9a010
Carson Katri <carson.katri@gmail.com>
2019-11-23 13:07:01 -0500

    Initial Commit

---
+++ ColorMenu/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  ColorMenu
+//
+//  Created by Carson Katri on 11/23/19.
+//  Copyright © 2019 Carson Katri. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
